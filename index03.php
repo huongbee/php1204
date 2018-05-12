c30be027f9c17724af26e950794b15aa150c5cdb
huong <huongnguyenak96@gmail.com>
2018-05-12 20:53:30 +0700

    ban co vua

---
+++ index03.php
@@ -0,0 +1,43 @@
+<?php
+
+$year = 1999;
+$can = '';
+$chi = '';
+// if($year%10==0){
+//     $can = "Canh";
+// }
+switch($year%10){
+    case 0 : $can = "Canh"; break;
+    case 1 : $can = "Tân"; break;
+    case 2 : $can = "Nhâm"; break;
+    case 3 : $can = "Quý"; break;
+    case 4 : $can = "Giáp"; break;
+    case 5 : $can = "Ất"; break;
+    case 6 : $can = "Bính"; break;
+    case 7 : $can = "Đinh"; break;
+    case 8 : $can = "Mậu"; break;
+    case 9 : $can = "Kỷ"; break;
+    
+}
+
+switch($year%12){
+    case 0 : $chi = "Thân"; break;
+    case 1 : $chi = "Dậu"; break;
+    case 2 : $chi = "Tuất"; break;
+    case 3 : $chi = "Hợi"; break;
+    case 4 : $chi = "Tí"; break;
+    case 5 : $chi = "Sửu"; break;
+    case 6 : $chi = "Dần"; break;
+    case 7 : $chi = "Mão"; break;
+    case 8 : $chi = "Thìn"; break;
+    case 9 : $chi = "Tỵ"; break;
+    case 10 : $chi = "Ngọ"; break;
+    case 11 : $chi = "Mùi"; break;
+}
+echo "Năm $year là năm $can $chi";
+
+// for($i=0; $i<=10; $i++){
+//     echo $i." ";
+// }   
+
+?>
---
+++ index04.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<style>
+    .banco{
+        width: 800px;
+        margin: 50px auto
+    }
+    .oco{
+        width: 100px;
+        height: 100px;
+        float: left;
+    }
+    .trang{
+        background-color: #fff
+    }
+    .den{
+        background-color: #000
+    }
+</style>
+<body>
+    <div class="banco">
+        <div class="oco trang"></div>
+        <div class="oco den"></div>
+        
+    </div>
+</body>
+</html>
